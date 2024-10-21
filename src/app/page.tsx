295e426edea274d05d42b173f6ad3b9dac8d5837
Bruno Novais <novais-bruno@hotmail.com>
2024-10-21 18:16:27 -0300

    Adicionado Analytics Vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-
+import { Analytics } from "@vercel/analytics/react";
 export default function Home() {
   return (
     <div>
+      <Analytics />
       <h1>Teste</h1>
       pagina de teste
     </div>

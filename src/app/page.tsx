853823b3aa47128d16ce6803357cbb2b88b6790a
Bruno Novais <novais-bruno@hotmail.com>
2024-10-23 15:10:23 -0300

    Feat Header and Frist Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
+import Header from "@/components/header/page";
 import { Analytics } from "@vercel/analytics/react";
 export default function Home() {
   return (
-    <div>
+    <div className="w-full h-screen flex flex-col justify-center items-center">
       <Analytics />
-      <h1>Teste</h1>
-      pagina de teste
+      <Header />
+      <div className="w-full flex justify-center items-center">
+        <div className="w-1/2 flex flex-col justify-center items-center px-10 py-6">
+          <div className="w-full flex flex-col text-left font-epilogue gap-6">
+            <strong className="text-sm" >Bem vindo ao </strong>
+            <h1 className="font-semibold text-6xl">Santa Clara Biscuit</h1>
+            <p>O melhor atelier de Volta Redonda , voltado para peças de biscuit e bolos fakes</p>
+          </div>
+          <div className="w-full flex justify-start items-start mt-3">
+            <button className="w-52 h-16 bg-black text-white font-epilogue">
+              <strong className="text-lg">Contato</strong>
+            </button>
+          </div>
+        </div>
+        <div className="w-1/2 flex justify-center items-center">
+          <img src="/Logo_Stb.png" alt="biscuit" className="w-96 h-96 object-cover" />
+        </div>
+
+      </div>
     </div>
   );
 }
---
+++ src/components/header/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <div className="w-full h-10 flex justify-between items-center px-10">
+      <div className="w-1/2 flex justify-start items-start">
+        <Image src="/logotipo.png" alt="logo" width={60} height={60} />
+      </div>
+      <div className="flex w-1/2 justify-end items-end">
+        <ul className="w-52 h-7 flex justify-center items-center gap-6 font-epilogue font-normal">
+          <li>About</li>
+          <li>Contact</li>
+          <li>Work</li>
+        </ul>
+      </div>
+    </div>
+  )
+}

f5ba8c07b6718474775f861403b642212534b82d
Bruno Novais <novais-bruno@hotmail.com>
2024-10-23 16:46:58 -0300

    Feath Mobile Layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,24 +5,25 @@ export default function Home() {
     <div className="w-full h-screen flex flex-col justify-center items-center">
       <Analytics />
       <Header />
-      <div className="w-full flex justify-center items-center Mobile: flex-col">
-        <div className="w-1/2 flex flex-col justify-center items-center px-10 py-6 Mobile:w-full">
+      <section className="w-full flex flex-col justify-center items-center p-2 lg:px-20 lg:py-6">
+        <div className="w-full h-auto flex flex-col justify-center items-center lg:flex-row">
           <div className="w-full flex flex-col text-left font-epilogue gap-6">
             <strong className="text-sm" >Bem vindo ao </strong>
             <h1 className="font-semibold text-6xl">Santa Clara Biscuit</h1>
             <p>O melhor atelier de Volta Redonda , voltado para peças de biscuit e bolos fakes</p>
+            <div className="w-full flex justify-start items-start mt-3">
+              <button className="w-52 h-16 bg-black text-white font-epilogue">
+                <strong className="text-lg">Contato</strong>
+              </button>
+            </div>
           </div>
-          <div className="w-full flex justify-start items-start mt-3">
-            <button className="w-52 h-16 bg-black text-white font-epilogue">
-              <strong className="text-lg">Contato</strong>
-            </button>
+
+          <div className="w-full flex justify-center items-center mt-5">
+            <img src="/Logo_Stb.png" alt="biscuit" className="w-96 h-96 object-cover" />
           </div>
-        </div>
-        <div className="w-1/2 flex justify-center items-center Mobile:w-full">
-          <img src="/Logo_Stb.png" alt="biscuit" className="w-96 h-96 object-cover" />
         </div>
 
-      </div>
+      </section>
     </div>
   );
 }
